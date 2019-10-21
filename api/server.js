c8d549771b138a21e33f5a0bd66bda1009c703f5
Nicholas Truson <nicholastruson@Jessicas-MacBook-Pro.local>
2019-10-21 11:25:35 -0700

    initial server configuration

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+//
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+//
+
+server.get('/', (req, res) => {
+    res.send('Server is running...');
+  });
+  
+  module.exports = server;
