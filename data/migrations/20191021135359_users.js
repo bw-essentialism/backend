0189dc5798541f5943773731750e49ae65ee33d7
Nicholas Truson <nicholastruson@Jessicas-MacBook-Pro.local>
2019-10-22 13:47:39 -0700

    user migration

--- data/migrations/20191021135359_users.js
+++ data/migrations/20191021135359_users.js
@@ -1,8 +1,19 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('users', table => {
+      table.increments();
+      table
+        .string('username', 32)
+        .notNullable()
+        .unique();
+      table.string('password', 32).notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.string('email', 32);
+      table.string('firstName', 32);
+      table.string('lastName', 32);
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+  };
   
-};
