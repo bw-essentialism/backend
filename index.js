53bd3d9fb5762347c4ed57a5d0458688046950ea
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-23 14:52:30 -0700

    ...

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 5500;
-server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
+server.listen(port, () => console.log(`server up on port ${port}`));
