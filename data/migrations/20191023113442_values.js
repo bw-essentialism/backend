e5ae8fa970de137c51fd048643ade35c3e5dfbd6
Nicholas Truson <nicholastruson@Jessicas-MacBook-Pro.local>
2019-10-23 11:43:53 -0700

    value migration and seed

--- data/migrations/20191023113442_values.js
+++ data/migrations/20191023113442_values.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('values', table => {
+      table.increments();
+      table
+        .string('value_name', 64)
+        .notNullable()
+        .unique();
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('values');
+  };
   
-};
--- data/seeds/002-values.js
+++ data/seeds/002-values.js
@@ -1,13 +1,48 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('values').insert([
+    {
+      value_name: 'Athletic ability'
+    },
+    {
+      value_name: 'Art and literature'
+    },
+    {
+      value_name:
+        'Creativity, discovering, or inventing things to make a difference in the world'
+    },
+    {
+      value_name: 'Independence'
+    },
+    {
+      value_name: 'Kindness and generosity'
+    },
+    {
+      value_name: 'Living in the moment'
+    },
+    {
+      value_name:
+        'Membership in a social group (such as your community, racial group, or school club)'
+    },
+    {
+      value_name: 'Music'
+    },
+    {
+      value_name: 'My community'
+    },
+    {
+      value_name: 'My moral principles'
+    },
+    {
+      value_name: 'Nature and the environment'
+    },
+    {
+      value_name: 'Relationships with friends and family'
+    },
+    {
+      value_name: 'Sense of humor'
+    },
+    {
+      value_name: 'Success in my career'
+    }
+  ]);
 };
