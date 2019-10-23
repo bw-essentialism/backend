9e86ea05e4c60b989c3ed3ccdd628a37d721ee80
Nicholas Truson <nicholastruson@Jessicas-MacBook-Pro.local>
2019-10-23 07:46:11 -0700

    values route added to server

--- api/server.js
+++ api/server.js
@@ -5,13 +5,14 @@ const helmet = require('helmet');
 const server = express();
 
 const authRouter = require('../routers/authRouter');
+const valuesRouter = require('../routers/valuesRouter');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-
+server.use('/api/values', valuesRouter);
 
 
 server.get('/', (req, res) => {
