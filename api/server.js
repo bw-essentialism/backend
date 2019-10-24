0a22727803f9b582c1c582336394fc4aff8c83ef
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-24 13:31:12 -0700

    proj model and route

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const server = express();
 const authRouter = require('../routers/authRouter');
 const valuesRouter = require('../routers/valuesRouter');
 const userValuesRouter = require('../routers/userValuesRouter');
+const projectsRouter = require('../routers/projectsRouter');
 
 server.use(helmet());
 server.use(express.json());
@@ -15,6 +16,7 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/values', valuesRouter);
 server.use('/api/uservalues', userValuesRouter);
+server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
     res.send('Server is running...');
