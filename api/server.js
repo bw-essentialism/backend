41d207855fe150fc7bee4b2f2c2c8336ea05b247
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-24 13:54:23 -0700

    getting h10 err again, trying to resolve

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const server = express();
 const authRouter = require('../routers/authRouter');
 const valuesRouter = require('../routers/valuesRouter');
 const userValuesRouter = require('../routers/userValuesRouter');
-const projectsRouter = require('../routers/projectsRouter');
+//const projectsRouter = require('../routers/projectsRouter');
 
 server.use(helmet());
 server.use(express.json());
@@ -16,7 +16,7 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/values', valuesRouter);
 server.use('/api/uservalues', userValuesRouter);
-server.use('/api/projects', projectsRouter);
+//server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
     res.send('Server is running...');
