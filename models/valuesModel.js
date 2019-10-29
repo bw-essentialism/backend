1f3776e2670c25552258bcf106d7d6f85ba9d6a8
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-29 12:22:21 -0700

    getting add and edit values to work

--- models/valuesModel.js
+++ models/valuesModel.js
@@ -1,7 +1,44 @@
+// db config
 const db = require('../utilities/dbConfig');
 
-const getValues = async () => {
-  return await db('values');
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove,
+    addUserValue
 };
 
-module.exports = { getValues };
+function find() {
+    return db('values')
+}
+
+function findById(id) {
+    return db('values')
+        .where({ id })
+        .first()
+}
+
+function add(payload) {
+    return db('values')
+        .insert(payload)
+}
+
+function update(id, payload) {
+    return db('values')
+        .where({ id })
+        .update(payload)
+}
+
+function remove(id) {
+    return db('values')
+        .where('id', id)
+        .del()
+}
+
+function addUserValue(userId, valueId) {
+    return db('users_values')
+        .insert({ user_id: userId, value_id: valueId })
+}
